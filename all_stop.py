23d708d247fb255781cb23dfe8bc6f7df77637e9
Joe-Costa <jcostaasc@me.com>
2024-01-04 14:46:05 -0800

    Added argparser group to all_stop.py

--- all_stop.py
+++ all_stop.py
@@ -7,23 +7,27 @@ import resume_cluster
 
 # This script is used to place a Qumulo cluster in read-only mode
 
+
 def main():
-    parser = argparse.ArgumentParser(description="Set Qumulo Cluster in Read-Only mode and recover it with --stop and --resume options")
-    parser.add_argument("--stop", action="store_true", help="Set Cluster to Read-Only")
-    parser.add_argument("--resume", action="store_true", help="Restore Cluster from Read-Only mode")
+    parser = argparse.ArgumentParser(
+        description="Set Qumulo Cluster in Read-Only mode with --stop and recover it the --resume option"
+    )
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("--stop", action="store_true", help="Set Cluster to Read-Only")
+    group.add_argument("--resume", action="store_false", help="Restore Cluster from Read-Only mode")
 
     args = parser.parse_args()
 
-    if args.stop:  
+    if args.stop:
         set_read_only.main()
 
     elif args.resume:
         resume_cluster.main()
 
-
     else:
         print("No valid option provided. Use --stop or --resume.")
         exit()
+
 
 if __name__ == "__main__":
     main()
